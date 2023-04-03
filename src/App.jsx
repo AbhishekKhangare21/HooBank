a36634a56209be9f5e3c4eef01cc57a7a48e6954
Abhishek Khangare <abhishekkhangare21@gmail.com>
2023-04-03 07:12:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-red-600">
+      <h1 className="bg-slate-100">hi</h1>
+    </div>
+  );
+};
+
+export default App;
